73b5bb6c38626a2d61f65681f948763db656c8eb
Mukesh <dhiman.mk19@gmail.com>
2025-05-01 11:15:00 +0530

    Mocked GPT response for local testing

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://aisitebuild.netlify.app'
+}));
+
 app.use(bodyParser.json());
 
 app.post('/generate', async (req, res) => {
